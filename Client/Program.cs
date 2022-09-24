48824e3de5fa055af5f943f9d9d5d976794a0f9f
Vinny Horgan <vinnyhorgan@gmail.com>
2022-09-24 13:27:37 +0200

    First commit

---
+++ Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using Raylib_cs;
+
+namespace Client
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Raylib.SetTraceLogLevel(TraceLogLevel.LOG_NONE);
+            Raylib.InitWindow(800, 600, "Project MMO");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+
+                Raylib.ClearBackground(Color.SKYBLUE);
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
